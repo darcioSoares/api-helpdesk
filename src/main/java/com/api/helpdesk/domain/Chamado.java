4c6939c8ac915e3b4071690662325065bdd0480e
darciosoares <darciosoares28@gmail.com>
2023-06-08 08:38:41 -0300

    classes de dominio, iniciadas e configuradas

---
+++ src/main/java/com/api/helpdesk/domain/Chamado.java
@@ -0,0 +1,60 @@
+package com.api.helpdesk.domain;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import com.api.helpdesk.domain.enums.Prioridade;
+import com.api.helpdesk.domain.enums.Status;
+
+public class Chamado {
+
+	private Integer id;
+	private LocalDate dataAbertura = LocalDate.now();
+	private LocalDate dataFechamento;
+	private Prioridade prioridade;
+	private Status status;
+	private String titulo;
+	private String observacao;
+	
+	private Tecnico tecnico;
+	private Cliente cliente;
+	
+	public Chamado() {
+		super();
+	}
+
+	public Chamado(Integer id, Prioridade prioridade, Status status, String titulo, String observacao, Tecnico tecnico,
+			Cliente cliente) {
+		super();
+		this.id = id;
+		this.prioridade = prioridade;
+		this.status = status;
+		this.titulo = titulo;
+		this.observacao = observacao;
+		this.tecnico = tecnico;
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Chamado other = (Chamado) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+	
+}
